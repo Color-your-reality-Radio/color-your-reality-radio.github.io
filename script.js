f7607b7787f3410b01a16b5f7c47c819371ab83f
HaydnTakesAminor <haydntakesaminor@gmail.com>
2026-03-07 16:55:31 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+// получаем кнопку Play
+const playButton = document.getElementById("playButton");
+
+// состояние плеера
+let isPlaying = false;
+
+// обработчик нажатия
+playButton.addEventListener("click", function () {
+
+    if (isPlaying) {
+
+        // если играло → ставим паузу
+        playButton.textContent = "▶";
+        isPlaying = false;
+
+    } else {
+
+        // если было на паузе → запускаем
+        playButton.textContent = "⏸";
+        isPlaying = true;
+
+    }
+
+});
